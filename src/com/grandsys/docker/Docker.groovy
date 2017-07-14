cd48d5ed839a44af7e7bf1d8f452b936d4d0e1c1
Henry Jao <jaohaohsuan@gmail.com>
2017-07-14 09:53:26 +0800

    attache 'return this' makes class statically

--- src/com/grandsys/docker/Docker.groovy
+++ src/com/grandsys/docker/Docker.groovy
@@ -3,3 +3,5 @@ package com.grandsys.docker
 def containerIP(container) {
     return sh(script: "docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${container.id}", returnStdout: true).trim()
 }
+
+return this
--- src/com/grandsys/git/Git.groovy
+++ src/com/grandsys/git/Git.groovy
@@ -1,3 +1,5 @@
 package com.grandsys.git
 
 def head = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+
+return this
