ff8a38b28583ec1b479bc76a1168eb9e6b6439da
Henry Jao <jaohaohsuan@gmail.com>
2017-07-14 10:56:22 +0800

    make Git methods statically

--- src/com/grandsys/git/Git.groovy
+++ src/com/grandsys/git/Git.groovy
@@ -1,5 +1,5 @@
 package com.grandsys.git
 
-def head = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
-
-return this
+class Git {
+	static def head = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+}
