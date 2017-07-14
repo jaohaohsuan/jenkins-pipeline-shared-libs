d50eee306135bc42772fbe1a55a72c4d92e7bd1b
Henry Jao <jaohaohsuan@gmail.com>
2017-07-14 13:00:25 +0800

    updates

--- src/com/grandsys/git/Git.groovy
+++ src/com/grandsys/git/Git.groovy
@@ -1,6 +1,7 @@
 package com.grandsys.git
 
-def test = 'test'
-def head = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
-
-return this
+class gitUtils {
+	static def head() {
+		return sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+	}
+}
--- vars/gitUtils.groovy
+++
@@ -1,5 +0,0 @@
-class gitUtils implements Serializable {
-	def head() {
-		sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
-	}
-}
