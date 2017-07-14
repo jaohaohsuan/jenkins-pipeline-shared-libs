c3881ac1ab482e422eaaf28c2c6c5084230d4b08
Henry Jao <jaohaohsuan@gmail.com>
2017-07-14 10:58:21 +0800

    undo

--- src/com/grandsys/git/Git.groovy
+++ src/com/grandsys/git/Git.groovy
@@ -1,5 +1,5 @@
 package com.grandsys.git
 
 class Git {
-	static def head = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+	def head = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
 }
